7333ecfa95382b73236f879dba43474eb54f8803
brenoafb <breno.afb@gmail.com>
2021-04-11 21:50:20 -0300

    Move code out of notebook, add type hints

--- ciqa.py
+++ ciqa.py
@@ -2,7 +2,7 @@ import numpy as np
 import PIL as pil
 import matplotlib.pyplot as plt
 
-def compress(image, block_size, levels):
+def compress(image: np.ndarray, block_size: int, levels: int) -> dict:
     array = np.asarray(image)
     (n_blocks, blocks) = get_blocks(array, block_size)
     quantized_blocks = [quantize(block, levels) for block in blocks]
@@ -13,7 +13,7 @@ def compress(image, block_size, levels):
         'quantized_blocks': quantized_blocks
     }
 
-def decompress(data):
+def decompress(data: dict) -> np.ndarray:
     n_blocks = data['blocks_per_row']
     block_size = data['block_size']
     quantized_blocks = data['quantized_blocks']
@@ -28,7 +28,7 @@ def decompress(data):
                 img[x+i,y+j] = block_img[i,j]
     return img
 
-def get_blocks(mat, block_size):
+def get_blocks(mat: np.ndarray, block_size: int) -> tuple:
     dim = mat.shape[0] # assume square matrix
     n_blocks = dim // block_size  # number of blocks per row
     blocks = []
@@ -42,7 +42,7 @@ def get_blocks(mat, block_size):
             blocks.append(block)
     return (n_blocks, blocks)
 
-def quantize(block, levels):
+def quantize(block: np.ndarray, levels: int) -> tuple:
     ranges = get_ranges(block, levels)
     quantized = np.zeros_like(block)
     for i in range(block.shape[0]):
@@ -50,7 +50,7 @@ def quantize(block, levels):
             quantized[i,j] = find_range(block[i,j], ranges)
     return (quantized, ranges)
 
-def get_ranges(block, levels):
+def get_ranges(block: np.ndarray, levels: int) -> list:
     lo = block.min()
     hi = block.max()
     range_size = (hi - lo) / levels
@@ -59,7 +59,7 @@ def get_ranges(block, levels):
     ]
     return ranges
 
-def find_range(value, ranges):
+def find_range(value: np.uint8, ranges: list) -> int:
     if value == ranges[-1][1]:
         return len(ranges) - 1
     for (i, r) in enumerate(ranges):
@@ -68,7 +68,7 @@ def find_range(value, ranges):
     print(f'Warning: returning none for value {value}, ({ranges})')
     return None
 
-def quantized_to_image(quantized, ranges):
+def quantized_to_image(quantized: np.ndarray, ranges: list) -> np.ndarray:
     img = np.zeros_like(quantized)
     midpoints = [(r[0] + r[1]) // 2 for r in ranges]
     for i in range(img.shape[0]):
