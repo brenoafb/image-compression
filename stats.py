1c586aacb994166c53c7345ee89092fd9dfa7c4d
brenoafb <breno.afb@gmail.com>
2021-04-24 10:08:48 -0300

    Add psnr calculations

--- stats.py
+++ stats.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def mse(a: np.ndarray, b: np.ndarray) -> float:
     return (np.square(a - b)).mean(axis=None)
+
+def psnr(mse, max=255):
+    return 20 * np.log10(max) - 10 * np.log10(mse)
