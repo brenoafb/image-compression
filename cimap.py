56ed5c24c5fd464abbd549a19eceed33ff086973
brenoafb <breno.afb@gmail.com>
2021-04-24 09:51:26 -0300

    Adjust arguments in compression function

--- cimap.py
+++ cimap.py
@@ -1,7 +1,7 @@
 import numpy as np
 from util import *
 
-def compress(image: np.ndarray, block_size: int, k: int) -> dict:
+def compress(image: np.ndarray, k: int) -> dict:
   size = image.shape[0] # assuming that image is square
   vectors = get_vectors(image)
   (codebook, labels) = get_codebook(vectors, k)
@@ -31,11 +31,4 @@ def build_image_from_codebook(codebook, labels, size):
       pixel = codebook[labels[i * size + j]]
       img[i][j] = pixel
 
-  # for p in range(n_blocks):
-  #     block = codebook[labels[p]].reshape((block_size, block_size))
-  #     x = (p // blocks_per_row) * block_size
-  #     y = (p % blocks_per_row) * block_size
-  #     for i in range(block_size):
-  #         for j in range(block_size):
-  #             img[x+i, y+j] = block[i, j]
   return img
