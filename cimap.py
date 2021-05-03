ff8bd62160ae2fe84a8603a763fcb45f6f40c99b
brenoafb <breno.afb@gmail.com>
2021-05-03 16:19:56 -0300

    Add CIMAP serialization

--- cimap.py
+++ cimap.py
@@ -1,5 +1,8 @@
 import numpy as np
+import json
+from base64 import b64encode, b64decode
 from util import *
+from math import ceil, log2
 
 def compress(image: np.ndarray, k: int) -> dict:
   size = image.shape[0] # assuming that image is square
@@ -17,6 +20,41 @@ def decompress(data: dict) -> np.ndarray:
   size = data['size']
 
   return build_image_from_codebook(codebook, labels, size)
+
+def serialize(data):
+    codebook_size = len(data['codebook'])
+    nbits = ceil(log2(codebook_size))
+    bits = []
+    (codebook_bits, codebook_shape) = bytes2bits(data['codebook'])
+    codebook_b64 = b64encode(codebook_bits.tobytes()).decode()
+    (labels_bits, labels_shape) = bytes2bits(data['labels'], element_bit_length=nbits)
+    labels_b64 = b64encode(labels_bits.tobytes()).decode()
+    size = data['size']
+    new_data = {'size': size,
+                'csize': codebook_size,
+                'cshape': codebook_shape,
+                'cb64': codebook_b64,
+                'lshape': labels_shape,
+                'lb64': labels_b64
+              }
+    return json.dumps(new_data)
+
+def deserialize(serialized):
+    data = json.loads(serialized)
+    nbits = ceil(log2(data['csize']))
+    
+    codebook_bits = bs.BitArray(b64decode(data['cb64']))
+    codebook = bits2bytes(codebook_bits, shape=data['cshape'])
+    labels_bits = bs.BitArray(b64decode(data['lb64']))
+    labels = bits2bytes(labels_bits, shape=data['lshape'], element_bit_length=nbits)
+    size = data['size']
+
+    new_data = {
+      'size': size,
+      'codebook': codebook,
+      'labels': labels
+    }
+    return new_data
 
 def get_vectors(array: np.ndarray) -> np.ndarray:
   r = array[:,:,0].flatten()
